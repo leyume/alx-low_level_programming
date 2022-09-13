66f6746ad949b89d8eba250241a135508a32ff1c
L eye <leyume@users.noreply.github.com>
2022-09-14 00:58:23 +0100

    Create 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+/*
+ * File: 101-natural.c
+ */
+#include "main.h"
+/**
+ * main - This code prints sum of multiples of 3 or 5
+ */
+
+int main(void)
+{
+    int x, result;
+
+    for (x=0; x < 1024; x++)
+    {
+        if( ( x % 5 == 0 ) || ( x % 3 == 0 ) )
+        {
+            result += x
+        }
+    }
+
+    _putchar(result);
+    _putchar('\n');
+
+}
