eb5b6ddb2ec3a590c45fa3f8fb24cc9745ed85a6
L eye <leyume@yahoo.com>
2022-09-13 18:08:28 -0700

    Updated: 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+/*
+ * File: 102-fibonacci.c
+ */
+#include "stdio.h"
+/**
+ * main - This code prints first 50 fibonacci numbers
+ * Return: Nothing
+ */
+int main(void)
+{
+	long int n1 = 1, n2 = 2, n;
+	int x;
+
+	printf("1, 2");
+
+	for (x = 3; x <= 98; x++)
+	{
+		n = n1 + n2;
+		n1 = n2;
+		n2 = n;
+		printf(", %ld", n);
+	}
+	putchar('\n');
+	return (0);
+}
