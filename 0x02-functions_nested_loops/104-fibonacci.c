48240318213816d267ec8e4c55a4765ff2091504
L eye <leyume@yahoo.com>
2022-09-14 13:33:05 -0700

    Updated: 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,17 +9,21 @@
 int main(void)
 {
 	unsigned long int i, j = 1, k = 2, j1, j2, k1, k2;
+
 	printf("%lu", j);
+
 	for (i = 1; i < 91; i++)
 	{
 		printf(", %lu", k);
 		k = k + j;
 		j = k - j;
 	}
+
 	j1 = j / 1000000000;
 	j2 = j % 1000000000;
 	k1 = k / 1000000000;
 	k2 = k % 1000000000;
+
 	for (i = 92; i < 99; ++i)
 	{
 		printf(", %lu", k1 + (k2 / 1000000000));
