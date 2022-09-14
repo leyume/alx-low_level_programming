346b8019ed0808c63ddb6b4516a088f30c03e6b4
L eye <leyume@yahoo.com>
2022-09-13 17:59:10 -0700

    Updated: 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-	int n1 = 1, n2 = 2, n;
+	int n1 = 1, n2 = 2, n, x;
 
 	printf("1, 2");
 
-	for (int x = 3; x < 50; x++)
+	for (x = 3; x < 50; x++)
 	{
 		n = n1 + n2;
 		n1 = n2;
