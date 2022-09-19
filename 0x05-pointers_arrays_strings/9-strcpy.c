1061df6047521ea714c1d1dddc718293af738950
L eye <leyume@users.noreply.github.com>
2022-09-19 18:46:30 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -16,13 +16,11 @@ char *_strcpy(char *dest, char *src)
 	int x = 0;
 
 	while (*(src + l) != '\0')
-	{
 		l++;
-	}
+	
 	for ( ; x < l ; x++)
-	{
 		dest[x] = src[x];
-	}
+	
 	dest[l] = '\0';
 	return (dest);
 }
