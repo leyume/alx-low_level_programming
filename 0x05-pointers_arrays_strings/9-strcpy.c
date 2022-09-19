9cab383f6f08eb3b6b7edc00051491ed9c471b3a
L eye <leyume@users.noreply.github.com>
2022-09-19 18:59:01 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,7 +4,7 @@
 /**
  * _strcpy - Copies a string pointed to by @src, including the terminating
  * null byte, to a buffer pointed to by @dest.
- * @dest: A buffer to copy the string to,  
+ * @dest: A buffer to copy the string to
  * @src: The source string to copy
  * Return: A pointer to the destination string @dest.
  */
