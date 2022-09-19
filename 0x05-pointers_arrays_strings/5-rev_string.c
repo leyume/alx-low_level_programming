ea858abab6548834c2d72547c6874c9f367d58bb
L eye <leyume@users.noreply.github.com>
2022-09-19 18:42:18 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * rev_string - Reverses a string.
- * @s: The string to be reversed.
+ * rev_string - function to reverses a string.
+ * @s - The string to be reversed.
  */
 
 void rev_string(char *s)
